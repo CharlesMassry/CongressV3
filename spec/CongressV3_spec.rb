50208ad30c827a2a94fcd362376f0ef62e8ef5ab
charlesmassry <me@charliemassry.com>
2016-04-13 14:28:58 -0400

    Fix Readme, setup and console scripts, add pry, and update travis file

--- spec/CongressV3_spec.rb
+++ spec/CongressV3_spec.rb
@@ -4,8 +4,4 @@ describe CongressV3 do
   it 'has a version number' do
     expect(CongressV3::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
